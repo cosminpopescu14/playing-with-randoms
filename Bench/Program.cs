6fe3ed257d1ead727edada143e483588ec9143c6
Cosmin Popescu <cosmin.popescu93@gmail.com>
2019-11-10 17:30:57 +0200

    joacă :)

--- Bench/Program.cs
+++ Bench/Program.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Bench
 {
@@ -11,23 +13,14 @@ namespace Bench
         private static readonly Random random = new Random();
         static void Main(string[] args)
         {
-            //int result = 0;
-
             var sw = new Stopwatch();
             sw.Start();
-
-            /*for (int i = 1000000000; i != 0; i--)
-            {
-                result++;
-            }
-            Console.WriteLine("Value: {0} Took: {1}s", result, sw.ElapsedMilliseconds / 1000F);*/
-
-         
 
             /*for (int steps = 0; steps < 10000000; ++steps)
             {
                 GenerateRandomsPair();
             }*/
+            Parse() ;
             Console.WriteLine("Took: {0}s", sw.ElapsedMilliseconds / 1000F);
             
         }
@@ -46,33 +39,30 @@ namespace Bench
                 int randomvalue = Math.Abs(BitConverter.ToInt32(rno, 0));
                 int randomvalue1 = Math.Abs(BitConverter.ToInt32(rno1, 0));
                 //Console.WriteLine("{0}     {1}",randomvalue, randomvalue1);
-                //WriteInFile(randomvalue, randomvalue1);
             }            
         }
 
         private static void GenerateRandomsPair()
         {
-            /*lock (random)
+            lock (random)
             {
                 float res = 0f;
                 int nb1 = random.Next(0, int.MaxValue);
                 int nb2 = random.Next(0, int.MaxValue);
-                //res = (float)nb1 / nb2;
-                Console.WriteLine("{0},{1}", nb1, nb2);
-            }*/
-
-            /*int nb1 = new XorShiftRandom().NextInt32();
-            int nb2 = new XorShiftRandom().NextInt32();
-            float res = (float)nb1 / nb2;*/
-
-            //WriteInFile();
-
+                res = (float)nb1 / nb2;
+                WriteInFile(nb1, nb2, res);
+            }
         }
 
         private static void WriteInFile(int nb1 = 0, int nb2 = 0, float res = 0)
         {
             var sb = new StringBuilder();
-            sb.Append("aaaaaaa");
+            sb.Append(nb1);
+            sb.Append("\t");
+            sb.Append(nb2);
+            sb.Append("\t");
+            sb.Append(res);
+            sb.Append("\n");
             File.AppendAllText("WriteTextAsync.txt", sb.ToString());
         }
 
@@ -80,9 +70,17 @@ namespace Bench
         {
             var path = @"C:\Users\cosmi\Desktop\random_numbers.txt";
 
-            
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    decimal x = decimal.Parse(line.Split("\t")[0]);
+                    decimal y = decimal.Parse(line.Split("\t")[1]);
 
-    
+                    decimal res = x / y;
+                }
+            }
         }
     }
 }
